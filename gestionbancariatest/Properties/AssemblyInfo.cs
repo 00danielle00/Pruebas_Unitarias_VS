be4e2a12acd1803a87045fe7789fa400bb2b9cb0
DESKTOP-AI1DKKS\Dani <danielleludovicoo@gmail.com>
2025-01-21 16:12:07 +0100

    commit

---
+++ gestionbancariatest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gestionbancariatest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gestionbancariatest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9342e2d-77e9-401b-966e-20f39ebced7e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ gestionbancariatest/gestionbancariatests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using gestionBancariaApp;
+
+namespace gestionbancariatest
+{
+    [TestClass]
+    public class gestionbancariatests
+    {
+        [TestMethod]
+        public void ValidarMetodoIngreso()
+        {
+            double saldoInicial = 1000;
+            double ingreso = 500;
+            double saldoActual = 0;
+            double saldoEsperado = 1500;
+
+            gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+            cuenta.realizarIngreso(ingreso);
+            saldoActual = cuenta.obtenerSaldo();
+
+            Assert.AreEqual(saldoEsperado, saldoActual, 0.001,"El saldo de la cuenta no es correcto");
+        }
+    }
+}
